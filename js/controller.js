e9d1a906f1ded3cda7d19c1e533791d40be9facf
Beck <rodrigue.beck@gmail.com>
2017-08-21 18:43:20 +0200

    Ajout du "$apply()" amélioré

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,14 @@
 var app = angular.module('uneApp', []);
 
 app.controller("uneFonctionCtrl", function ($scope, $rootScope,$timeout, WebSocketService) {
-	
+
 //Mise en place du Socket
 	$rootScope.ws = new SocketManager.SocketManager($scope,$rootScope);
-	
+
 	$scope.foo = [
 	              {username: "rbeck", content:"bienvenue"},
 	              {username: "sthomas", content:"WTF"}];
-	
+
 	$scope.fireWStest = function(){
 //		$rootScope.ws.send("test");
 		var unObjetJson = {
@@ -35,36 +35,40 @@ app.factory("Post", function(){
 	return factory;
 })
 /**
- * Apparition d'�l�ments suite � une "event"
+ * Apparition d'éléments / Tx serveur,
+ * A lancer lorsque l'on souhaite MAJ une page sous AngularJS...
  */
-//MAJ des listes de cat�gorie
-//	$scope.majCatego = function (list) {
-//		$timeout(function () {
-//			$scope.categories = list;
-//		}, 100);
-//	}
+$scope.safeApply = function() {
+	var phase = this.$root.$$phase;
+	if(phase == '$apply' || phase == '$digest')
+	 this.$eval();
+	else
+	 this.$apply();
+};
+
+
 var SocketManager = {
 	ws:{},
-	
+
 	SocketManager: function (scope,rootScope){
         this.ws = new WebSocket("ws://localhost:9999/");
 //        this.ws = new WebSocket("ws://localhost:80/");
 //        this.ws = new WebSocket("ws://172.28.50.187/");//ne fonctionne pas chez Ali le: 28/1/2016
-        
+
         this.ws.onopen = function(){
         	console.log("Socket has been opened!");
         };
-        
+
     	this.ws.onmessage = function(message) {
 //console.log(message);
     	    var messageObj = JSON.parse(message.data);
-    	    
+
   console.log("Received data from websocket: ", messageObj);
     	    rxTools.rxRouting(scope,rootScope,messageObj);
     	};
-        
+
         return this.ws;
     }
-	
+
 };
 //TODO @ICI: vid�o n�7 � 0:02:00 pour commencer � tester "$http" et "$q"
